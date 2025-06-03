e7b03ffb8787b3a8a72885ce45367ddc28a905d5
Kousalya Potti <kousalyapotti1@gmail.com>
2025-06-03 15:54:37 -0400

    problem 4

---
+++ session_1/standard_set_1/problem4.py
@@ -0,0 +1,15 @@
+def get_item(items, x):
+    if x in items:
+        #return[x]
+        print(get_item[x])
+    else:
+        #return None
+        print("doesnt work")
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 2
+get_item(items, x)
+
+items = ["piglet", "pooh", "roo", "rabbit"]
+x = 5
+get_item(items, x)
