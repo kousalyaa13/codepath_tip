752048afb0cfb4892f996427e1047d2853cc447a
Kousalya Potti <kousalyapotti1@gmail.com>
2025-09-29 23:59:16 -0400

    .

--- Breakout Problems Session 2/Standard Problem Set Version 1/Merge Strings Alternately.py
+++ Breakout Problems Session 2/Standard Problem Set Version 1/Merge Strings Alternately.py
@@ -1,21 +1,27 @@
 def merge_alternately(word1, word2):
-    combined = []
+    merged = []
+    # initialize pointers
     i = 0
     j = 0
-    
+
+    # while the length of the words are less than the pointers
     while i < len(word1) and j < len(word2):
-        combined.append(word1[i])
-        combined.append(word2[j])
+        # keep alternatively merging
+        merged.append(word1[i])
+        merged.append(word2[j])
         i += 1
         j += 1
-
-    if i < len(word1):
-        combined.append(word1[i:])
-    if j < len(word2):
-        combined.append(word2[j:])
     
-    return "".join(combined)
+    # if there are still leftover letters as 
+    # the length is larger than the pointers
+    if i < len(word1):
+        # append the rest of the word
+        merged.append(word1[i:])
+    if j < len(word2):
+        merged.append(word2[j:])
 
+    return "".join(merged)
+   
 word1 = "wol"
 word2 = "oze"
 print(merge_alternately(word1, word2))
