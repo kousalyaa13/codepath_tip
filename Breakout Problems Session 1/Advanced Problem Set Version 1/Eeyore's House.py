c7c1e037cd807af137d3e52503774f978af55f27
Kousalya Potti <kousalyapotti1@gmail.com>
2025-09-30 00:21:02 -0400

    .

--- Breakout Problems Session 1/Advanced Problem Set Version 1/Eeyore's House.py
+++ Breakout Problems Session 1/Advanced Problem Set Version 1/Eeyore's House.py
@@ -1,9 +1,9 @@
-
 def good_pairs(pile1, pile2, k):
     count = 0
-    for i in pile1:
-        for j in pile2:
-            if i % (j * k) == 0:
+    
+    for i in range(len(pile1)):
+        for j in range(len(pile2)):
+            if pile1[i] % (pile2[j] * k) == 0:
                 count += 1
     return count
 
--- Breakout Problems Session 2/Standard Problem Set Version 2/Count Digits.py
+++ Breakout Problems Session 2/Standard Problem Set Version 2/Count Digits.py
@@ -1,10 +1,12 @@
 def count_digits(n):
     count = 0
+    
+    # special case
     if n == 0:
         return 1
-    
+
     while n > 0:
-        n //= 10 # floor division
+        n = n // 10
         count += 1
     return count
 
